c7a471dacca76063c9ee25266ed0f750dc94ee8c
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2023-10-09 16:40:46 -0600

    Completed Wheel Page

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,24 +1,47 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, SET_QUIZ_INTO_STATE, INPUT_CHANGE, RESET_FORM } from './action-types';
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch (action.type) {
+    case (MOVE_CLOCKWISE):
+      return (state = (state === 5 ? 0 : state + 1));
+    case (MOVE_COUNTERCLOCKWISE):
+      return (state = (state === 0 ? 5 : state - 1));
+    default: 
+      return state;
+  }
 }
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
-  return state
+  switch (action.type) {
+    // case (SET_SELECTED_ANSWER):
+    //   return (null);
+    default: 
+      return state;
+  }
 }
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch (action.type) {
+    case (SET_SELECTED_ANSWER):
+      return (null);
+    default: 
+      return state;
+  }
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch (action.type) {
+    case (SET_INFO_MESSAGE):
+      return (null);
+    default: 
+      return state;
+  }
 }
 
 const initialFormState = {
@@ -27,7 +50,16 @@ const initialFormState = {
   newFalseAnswer: '',
 }
 function form(state = initialFormState, action) {
-  return state
+  switch (action.type) {
+    // case test:
+    //   return {
+    //     ...state, 
+        
+    //   }
+    default: 
+      return state;
+  }
 }
 
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
+const reducer = combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form });
+export default reducer;
